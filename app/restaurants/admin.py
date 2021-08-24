160afc6d05cbf98335e133aa0309b53a7f84de58
Eduardo Herrera <eduherrer8@gmail.com>
2021-08-23 19:44:15 -0500

    adds the restaurant model adjust the dockerfile and docker-compose

--- app/restaurants/admin.py
+++ app/restaurants/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
 
-# Register your models here.
+from restaurants.models import Restaurant
+
+
+class RestaurantAdmin(OSMGeoAdmin):
+    list_display = ("id", "name", "raiting", "city")
+
+
+admin.site.register(Restaurant, RestaurantAdmin)
