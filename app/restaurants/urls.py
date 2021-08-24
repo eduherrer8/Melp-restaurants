a8c3d1cf37b6c80f3f49fb2dd432efa21efb8d8c
Eduardo Herrera <eduherrer8@gmail.com>
2021-08-24 12:15:25 -0500

    adds statistics enpoint

--- app/restaurants/urls.py
+++ app/restaurants/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from restaurants.views import RestaurantView
+from restaurants.views import Statistics
 
 restaurant_router = routers.SimpleRouter()
 restaurant_router.register(
@@ -11,5 +12,6 @@ restaurant_router.register(
 app_name = "restaurants"
 
 urlpatterns = [
+    path("restaurants/statistics", Statistics.as_view(), name="statistics"),
     path("", include(restaurant_router.urls)),
 ]
