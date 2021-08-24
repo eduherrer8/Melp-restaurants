7a775081eebb967d6a54baf282023a6151d3697a
Eduardo Herrera <eduherrer8@gmail.com>
2021-08-24 17:13:45 -0500

    try7

--- app/melp/settings.py
+++ app/melp/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("ENVIRONMENT") == "local"
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 ALLOWED_HOSTS = [
     "*"] if DEBUG else os.getenv("ALLOWED_EXTERNAL_HOSTS", "").split(",")
 
